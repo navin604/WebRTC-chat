774d32ec84337673cc80783986c170fa48219dfd
Navin Parmar <navinparmar98@gmail.com>
2023-02-12 13:45:12 -0800

    Added setRoomName action

--- src/Store/Actions/callActions.js
+++ src/Store/Actions/callActions.js
@@ -1,12 +1,20 @@
 export const SET_CALL_STATE = "SET.CALL_STATE";
+export const SET_ROOM_NAME = "SET.ROOM_NAME";
 
 export const callStates = {
   DISCONNECTED: "DISCONNECTED",
+  CONNECTED: "CONNECTED",
 };
 
 export const setCallState = (callState) => {
   return {
     type: SET_CALL_STATE,
     callState,
+  };
+};
+export const setRoomName = (roomName) => {
+  return {
+    type: SET_ROOM_NAME,
+    roomName,
   };
 };
