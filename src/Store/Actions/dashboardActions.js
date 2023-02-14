34c4baecf9b8c6482494d59101874b91d213e857
Navin Parmar <navinparmar98@gmail.com>
2023-02-13 18:35:20 -0800

    Removed Redux

--- src/Store/Actions/dashboardActions.js
+++
@@ -1,8 +0,0 @@
-export const DASHBOARD_SET_USERNAME = "DASHBOARD.SET_USERNAME";
-
-export const setUsername = (username) => {
-  return {
-    type: DASHBOARD_SET_USERNAME,
-    username,
-  };
-};
--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
 import App from "./App";
-import store from "./Store/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
-);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
