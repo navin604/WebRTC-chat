4433fe89c35227c1e93f00356444dc17aea1e773
Navin Parmar <navinparmar98@gmail.com>
2023-02-13 18:35:33 -0800

    Removed files

--- src/Store/reducer.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-import dashboardReducer from "./Reducers/dashboardReducer";
-import callReducer from "./Reducers/callReducer";
-export default combineReducers({
-  dashboard: dashboardReducer,
-  call: callReducer,
-});
