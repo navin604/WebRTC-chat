cee60cadd139fb4f3d80de274314f7910dfc017d
Navin Parmar <navinparmar98@gmail.com>
2023-02-16 23:56:22 -0800

    Added useEffect to connect with server when site loads

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Entry from "./Components/PreJoinScreen/preJoinScreen";
+import { connectWithServer } from "./utils/SocketClient";
 
 const App = () => {
+  useEffect(() => {
+    connectWithServer();
+  }, []);
   return (
     <div>
       <Entry />
