72538ca1cd7a3ed9b15a93d23848896a350c6f14
Navin Parmar <navinparmar98@gmail.com>
2023-02-19 02:01:33 -0800

    Added: Functionality for p2p calls

--- src/Components/p2pRooms/p2pCall/p2pCall.js
+++ src/Components/p2pRooms/p2pCall/p2pCall.js
@@ -3,19 +3,29 @@ import { connect } from "react-redux";
 import P2PRoomButton from "../p2pRoomButton/p2pRoomButton";
 import * as p2pCallHandler from "../../../utils/GroupCallHandler";
 import P2PRoom from "../p2pRoom";
+import {
+  setCameraEnabled,
+  setMicEnabled,
+} from "../../../store/Actions/CallActions";
 
 const P2PCall = (props) => {
-  const { callState, localstream } = props;
+  const { callState, localstream, p2pCallStreams, p2pCallActive } = props;
   const createRoom = () => {
     //create room
     console.log("Created group");
     p2pCallHandler.createP2PCall();
   };
 
+  const leaveRoom = () => {
+    //leave room
+    p2pCallHandler.leaveCall();
+  };
+
   return (
     <>
       <P2PRoomButton onClickHandler={createRoom} label="Create Room" />
-      <P2PRoom />
+      <P2PRoom {...props} />
+      <P2PRoomButton onClickHandler={leaveRoom} label="Leave Room" />
     </>
   );
 };
@@ -24,4 +34,11 @@ const mapStateToProps = ({ call }) => ({
   ...call,
 });
 
-export default connect(mapStateToProps)(P2PCall);
+const mapActionsToProps = ({ dispatch }) => {
+  return {
+    cameraToggle: (enabled) => dispatch(setCameraEnabled(enabled)),
+    micToggle: (enabled) => dispatch(setMicEnabled(enabled)),
+  };
+};
+
+export default connect(mapStateToProps, mapActionsToProps)(P2PCall);
--- src/Components/p2pRooms/p2pRoom.js
+++ src/Components/p2pRooms/p2pRoom.js
@@ -1,13 +1,19 @@
 import React from "react";
 import "./p2pRoom.css";
+import P2PStream from "./p2pStream/p2pStream";
 
-const P2PRoom = () => {
+const P2PRoom = (props) => {
+  const { p2pCallStreams } = props;
   return (
     <div className="group_call_room_container">
       <span className="group_call_title">Group CAll</span>
-      <div className="group_call_videos_container">Display streams</div>
+      <div className="group_call_videos_container">
+        {p2pCallStreams.map((stream) => {
+          return <P2PStream key={stream.id} stream={stream} />;
+        })}
+      </div>
     </div>
-    //AddToolBar
+    //AddToolBar with props
   );
 };
 
