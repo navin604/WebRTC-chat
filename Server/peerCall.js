850067111bfacf971220c360e2612e3c286c6944
navin604 <55507003+navin604@users.noreply.github.com>
2023-03-05 01:28:57 -0800

    Delete Server directory

--- Server/peerCall.js
+++
@@ -1,9 +0,0 @@
-const createPeerServerListeners = (peerServer) => {
-  peerServer.on("connection", (client) => {
-    console.log(`${client.id} connected to server`);
-  });
-};
-
-module.exports = {
-  createPeerServerListeners,
-};
