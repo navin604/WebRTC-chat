c253bbd80ada4267f947b381d3e4ab39846a82d0
Navin Parmar <navinparmar98@gmail.com>
2023-03-08 15:38:13 -0800

    Added conditional rendering

--- src/Components/VideoRoom/Toolbar/ToolBar.js
+++ src/Components/VideoRoom/Toolbar/ToolBar.js
@@ -4,23 +4,55 @@ import {
   faMicrophone,
   faPhoneSlash,
   faVideo,
+  faMicrophoneSlash,
+  faVideoSlash,
 } from "@fortawesome/free-solid-svg-icons";
 import "./Toolbar.css";
+import { connect } from "react-redux";
 
-const ToolBar = () => {
+const ToolBar = (props) => {
+  const {
+    microphoneEnabled,
+    cameraEnabled,
+    handleDisconnect,
+    handleVideoToggle,
+    handleAudioToggle,
+  } = props;
   return (
     <div className="toolbar_items">
-      <div className="toolbar_icon">
-        <FontAwesomeIcon icon={faMicrophone} />
+      <div
+        className={"toolbar_icon " + (!microphoneEnabled ? "disable" : "")}
+        onClick={handleAudioToggle}
+      >
+        <FontAwesomeIcon
+          icon={!microphoneEnabled ? faMicrophoneSlash : faMicrophone}
+          title={microphoneEnabled ? "Mute Audio" : "Unmute Audio"}
+        />
       </div>
-      <div className="toolbar_icon">
-        <FontAwesomeIcon icon={faVideo} />
+
+      <div
+        className={"toolbar_icon " + (!cameraEnabled ? "disable" : "")}
+        onClick={handleVideoToggle}
+      >
+        <FontAwesomeIcon
+          icon={!cameraEnabled ? faVideoSlash : faVideo}
+          title={cameraEnabled ? "Disable Video" : "Enable Video"}
+        />
       </div>
-      <div className="toolbar_icon">
-        <FontAwesomeIcon icon={faPhoneSlash} />
+
+      <div className="toolbar_icon disconnect">
+        <FontAwesomeIcon
+          icon={faPhoneSlash}
+          title="Disconnect"
+          onClick={handleDisconnect}
+        />
       </div>
     </div>
   );
 };
 
-export default ToolBar;
+const mapStatetoProps = ({ call }) => ({
+  ...call,
+});
+
+export default connect(mapStatetoProps)(ToolBar);
