2299adc264bf438d3f65ca660abb55df4281702d
Navin Parmar <navinparmar98@gmail.com>
2023-03-08 15:36:44 -0800

    Cleanup

--- src/Components/p2pRooms/p2pCall/p2pCall.js
+++ src/Components/p2pRooms/p2pCall/p2pCall.js
@@ -1,31 +1,11 @@
 import React from "react";
 import { connect } from "react-redux";
-import P2PRoomButton from "../p2pRoomButton/p2pRoomButton";
-import * as p2pCallHandler from "../../../utils/GroupCallHandler";
 import P2PRoom from "../p2pRoom";
-import {
-  setCameraEnabled,
-  setMicEnabled,
-} from "../../../store/Actions/CallActions";
 
 const P2PCall = (props) => {
-  const { callState, localstream, p2pCallStreams, p2pCallActive } = props;
-  const createRoom = () => {
-    //create room
-    console.log("Created group");
-    p2pCallHandler.createP2PCall();
-  };
-
-  const leaveRoom = () => {
-    //leave room
-    p2pCallHandler.leaveCall();
-  };
-
   return (
     <>
-      <P2PRoomButton onClickHandler={createRoom} label="Create Room" />
       <P2PRoom {...props} />
-      <P2PRoomButton onClickHandler={leaveRoom} label="Leave Room" />
     </>
   );
 };
@@ -34,11 +14,4 @@ const mapStateToProps = ({ call }) => ({
   ...call,
 });
 
-const mapActionsToProps = ({ dispatch }) => {
-  return {
-    cameraToggle: (enabled) => dispatch(setCameraEnabled(enabled)),
-    micToggle: (enabled) => dispatch(setMicEnabled(enabled)),
-  };
-};
-
-export default connect(mapStateToProps, mapActionsToProps)(P2PCall);
+export default connect(mapStateToProps)(P2PCall);
