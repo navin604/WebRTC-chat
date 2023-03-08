20c7bf1c86de5f324c6fcf355fc0f9ed2bdf6f73
Navin Parmar <navinparmar98@gmail.com>
2023-03-08 14:32:15 -0800

    Removed leave p2pcall button, moved it to toolbar

--- src/Components/p2pRooms/p2pRoomList/p2pRoomList.js
+++ src/Components/p2pRooms/p2pRoomList/p2pRoomList.js
@@ -1,27 +1,20 @@
 import React from "react";
 import P2PListItem from "./p2pListItem";
 import { connect } from "react-redux";
-import "./p2pRoomList.css"
+import "./p2pRoomList.css";
 import P2PRoomButton from "../p2pRoomButton/p2pRoomButton";
 // import P2PRoom from "../p2pRoom";
 import * as p2pCallHandler from "../../../utils/GroupCallHandler";
 // import Participants from "../../VideoRoom/Participants/Participants";
-import { callStatus } from "../../../store/Actions/CallActions"
+import { callStatus } from "../../../store/Actions/CallActions";
 
 const P2PRoomList = (props) => {
   const { p2pCallRooms, roomName, callState } = props;
   const createRoom = () => {
     //create room
-    console.log("Created group");
+    console.log("Created p2p call");
     p2pCallHandler.createP2PCall();
   };
-
-  const leaveRoom = () => {
-    //leave room
-    console.log("Leave group");
-    p2pCallHandler.leaveCall();
-  };
-
 
   return (
     <div className="room-list-wrapper">
@@ -29,20 +22,15 @@ const P2PRoomList = (props) => {
         {roomName === null && callState === callStatus.CALL_AVAILABLE && (
           <P2PRoomButton onClickHandler={createRoom} label="Create Room" />
         )}
-
-        {callState === callStatus.CALL_UNAVAILABLE && (
-          <P2PRoomButton onClickHandler={leaveRoom} label="Leave Room" />
-        )}
       </div>
       <div className="room-list">
         {p2pCallRooms.map((item) => (
-        <P2PListItem key={item.roomID} room={item} />
-      ))}
+          <P2PListItem key={item.roomID} room={item} />
+        ))}
       </div>
     </div>
   );
 };
-
 
 const mapStateToProps = ({ dashboard, call }) => ({
   ...dashboard,
@@ -50,4 +38,3 @@ const mapStateToProps = ({ dashboard, call }) => ({
 });
 
 export default connect(mapStateToProps)(P2PRoomList);
-
