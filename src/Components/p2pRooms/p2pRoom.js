4a5092b01df5c849425673160be571f0bd6acf86
Navin Parmar <navinparmar98@gmail.com>
2023-03-08 15:37:11 -0800

    Added styling for p2pRoom

--- src/Components/p2pRooms/p2pRoom.js
+++ src/Components/p2pRooms/p2pRoom.js
@@ -1,19 +1,31 @@
 import React from "react";
 import "./p2pRoom.css";
 import P2PStream from "./p2pStream/p2pStream";
+import "../VideoRoom/Participants/Participants.css"
+
 
 const P2PRoom = (props) => {
   const { p2pCallStreams } = props;
+
+
+  let gridCol =
+    p2pCallStreams.length === 1 ? 1 : p2pCallStreams.length <= 4 ? 2 : 4;
+  const gridColSize = p2pCallStreams.length <= 4 ? 1 : 2;
+  let gridRowSize =
+    p2pCallStreams.length <= 4
+      ? p2pCallStreams.length
+      : Math.ceil(p2pCallStreams.length / 2);
+
   return (
-    <div className="group_call_room_container">
-      <span className="group_call_title">Group CAll</span>
-      <div className="group_call_videos_container">
-        {p2pCallStreams.map((stream) => {
-          return <P2PStream key={stream.id} stream={stream} />;
-        })}
-      </div>
+    <div  style={{
+      "--grid-size": gridCol,
+      "--grid-col-size": gridColSize,
+      "--grid-row-size": gridRowSize,
+    }} className="participants">
+      {p2pCallStreams.map((stream) => {
+        return <P2PStream key={stream.id} stream={stream} />;
+      })}
     </div>
-    //AddToolBar with props
   );
 };
 
