eb6a97195bb7134e03206bff69025e1815dc3649
Navin Parmar <navinparmar98@gmail.com>
2023-03-08 23:21:19 -0800

    Added debounce to prevent users from spam joining a room

--- src/Components/p2pRooms/p2pRoomList/p2pListItem.js
+++ src/Components/p2pRooms/p2pRoomList/p2pListItem.js
@@ -1,13 +1,15 @@
 import React from "react";
 import * as GroupCallHandler from "../../../utils/GroupCallHandler";
-import "./p2pRoomListItem.css"
+import "./p2pRoomListItem.css";
+import debounce from "lodash.debounce";
 const P2PListItem = ({ room }) => {
   const handleJoinRoom = () => {
     //Handle shit
     GroupCallHandler.joinP2PCall(room.socketID, room.roomID);
   };
+  const onClick = debounce(handleJoinRoom, 300, true);
   return (
-    <div className='group_calls_list_item' onClick={handleJoinRoom}>
+    <div className="group_calls_list_item" onClick={onClick}>
       <span>{room.hostname}</span>
     </div>
   );
